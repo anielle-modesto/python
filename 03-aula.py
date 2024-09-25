48fc23160a2f2639a8f8fb0642538a52e7c68185
Anielle Modesto <aniellebarbosadamotamodesto@gmail.com>
2024-09-25 10:38:53 -0300

    Create 03-aula.py

---
+++ 03-aula.py
@@ -0,0 +1,16 @@
+#variáveis em Python
+#não há necessidade de declará-las no inicio do programa
+#basta criar no meio do algoritmo com a atribuição
+
+teste = "Anielle" 
+print(teste)  # imprime a string "Julia"
+print(type(teste)) #imprime o tipo da variável,string
+teste = 16 
+print(teste) #imprime o número 16
+print(type(teste)) #imprime o tipo da variável, que é int
+teste = 1,70
+print(teste) #imprime o número 1,70
+print(type(teste)) #imprime oo tipo da variável que é float
+teste = True
+print(teste) #imprime o valor True
+print(type(teste)) #imprime o tipo da variável,que é bool
